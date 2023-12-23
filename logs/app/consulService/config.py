33f68efcfefb2acaf30711e7505dedc622e8337c
Aritz Domaika <aritz.domaika@alumni.mondragon.edu>
2023-12-23 11:24:42 +0100

    modified port sent to consul

--- logs/app/consulService/config.py
+++ logs/app/consulService/config.py
@@ -1,6 +1,7 @@
 from os import environ
 from dotenv import load_dotenv
 import ifaddr
+import requests
 
 # Only needed for developing, on production Docker .env file is used
 load_dotenv()
@@ -11,7 +12,7 @@ class Config:
     CONSUL_HOST = environ.get("CONSUL_HOST", "192.168.18.201")
     CONSUL_PORT = environ.get("CONSUL_PORT", 8500)
     CONSUL_DNS_PORT = environ.get("CONSUL_DNS_PORT", 8600)
-    PORT = int(environ.get("UVICORN_PORT", '8001'))
+    PORT = int(environ.get("LOG_PORT", '18010'))
     SERVICE_NAME = environ.get("SERVICE_NAME", "logs")
     SERVICE_ID = environ.get("SERVICE_ID", "logs1")
     IP = None
@@ -33,7 +34,19 @@ class Config:
             Config.__instance = self
 
     def get_ip(self):
-        ip = Config.get_adapter_ip("eth0")  # this is the default interface in docker
+        # ip = Config.get_adapter_ip("eth0")  # this is the default interface in docker
+
+        url_token = "http://169.254.169.254/latest/api/token"
+        headers = {"X-aws-ec2-metadata-token-ttl-seconds": "21600"}
+        response = requests.put(url_token, headers=headers)
+        token = response.content.decode('utf-8')
+
+        # Usa el token para obtener la IP pública
+        url_ip = "http://169.254.169.254/latest/meta-data/local-ipv4"
+        headers = {"X-aws-ec2-metadata-token": token}
+        respuesta = requests.get(url_ip, headers=headers)
+        ip = respuesta.content.decode('utf-8')
+    
         if ip is None:
             ip = "127.0.0.1"
         self.IP = ip
